93067201b2cd3bdf9ebc56ccc68b4a66762ba17f
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-11 22:17:11 +0600

    registration doen

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,28 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [inputError, setInputError] = useState("")
+    const [inputEmailError, setInputEmailError] = useState("")
+    const [inputPasswordError, setInputPasswordError] = useState("")
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const getEmail = event => {
+        const inputEmail = event.target.value
+        if (/\S+@\S+\.\S+/.test(inputEmail)) {
+            setEmail(inputEmail)
+            setInputEmailError("")
+        }
+        else { setInputEmailError("Please provide a valid email") }
+    }
+
+    const getPassword = event => {
+        const inputPassword = event.target.value
+
+
+        if (inputPassword.length >= 6) {
+            setPassword(inputPassword)
+            setInputError("")
+
+        }
+        else {
+            setInputError("Invalid Password")
+        }
+    }
+    const getConfirmPassword = event => {
+        const inputPassword = event.target.value
+
+
+        if (inputPassword === password) {
+            setPassword(inputPassword)
+            setInputPasswordError("")
+
+        }
+        else {
+            setInputPasswordError("password mitch match")
+        }
+    }
+
+    const handleRegister = event => {
+        event.preventDefault()
+        createUserWithEmailAndPassword(email, password)
+        navigate("/")
+        toast.success("user created")
+
+    }
+    if (error) {
+        toast.error(error.message)
+    }
+    useEffect(() => {
+        if (user) {
+            toast.success(user.email)
+        }
+    })
     return (
         <div className='w-50 mx-auto text-white'>
             <h1 className='text-center'>Register please</h1>
-            <Form>
+            <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicFullName">
                     <Form.Label>Full name</Form.Label>
                     <Form.Control type="text" placeholder="Full name" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-
+                    <Form.Control type="email" placeholder="Enter email" onChange={getEmail} />
+                    <span className='text-danger'>{inputEmailError ? inputEmailError : ""}</span>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={getPassword} />
+                    <span className='text-danger'>{inputError ? inputError : ""}</span>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control type="password" placeholder="Confirm Password" onChange={getConfirmPassword} />
+                    <span className='text-danger'>{inputPasswordError ? inputPasswordError : ""}</span>
                 </Form.Group>
                 <Button className='w-25 bg-primary' type="submit">
                     Register
